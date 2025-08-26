0d8c75e296f9da4a320f967c6ec7cb6885055a58
Tommyleem1 <tlm4219@gmail.com>
2025-08-26 15:16:33 +1000

    Delete card.jsx

--- card.jsx
+++
@@ -1,6 +0,0 @@
-export function Card({ className = "", children }) {
-  return <div className={`bg-white border ${className}`}>{children}</div>;
-}
-export function CardContent({ className = "", children }) {
-  return <div className={className}>{children}</div>;
-}
