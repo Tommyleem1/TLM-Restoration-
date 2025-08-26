b828c3671095296b1e1bac5a2ff77ec2396b6d22
Tommyleem1 <tlm4219@gmail.com>
2025-08-26 16:41:20 +1000

    Update layout.jsx

--- tlm-restoration-next/app/layout.jsx
+++ tlm-restoration-next/app/layout.jsx
@@ -1,17 +1,14 @@
 import '../styles/globals.css';
-  title: "TLM Restoration | Restoring Properties. Restoring Peace of Mind.",
-  description: "24/7 Emergency restoration services across Greater Melbourne and Victoria.",
+
+export const metadata = {
+  title: 'TLM Restoration | Restoring Properties. Restoring Peace of Mind.',
+  description: '24/7 Emergency restoration services across Greater Melbourne and Victoria.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white">
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-        <link href="/styles.css" rel="stylesheet" />
-        <link rel="stylesheet" href="/styles/globals.css" />
+      <body>
         {children}
       </body>
     </html>
